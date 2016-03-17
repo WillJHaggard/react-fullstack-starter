c30d70ecd2dcd6957073011017793038ccfe78fb
WillJHaggard <willjhaggard@gmail.com>
2016-03-17 09:29:30 -0400

    Added body component and imports for ux & soft

---
+++ frontend/components/softEng.js
@@ -0,0 +1,9 @@
+import softEngAdd from "./add/softEng";
+import softEngDetail from "./detail/softEng";
+import softEngEdit from "./edit/softEng";
+import softEngIndex from "./index/softEng";
+import softEngItem from "./item/softEng";
+
+export default {
+  softEngAdd, softEngDetail, softEngEdit, softEngIndex, softEngItem
+};
---
+++ frontend/components/uxEng.js
@@ -0,0 +1,9 @@
+import uxEngAdd from "./add/uxEng";
+import uxEngDetail from "./detail/uxEng";
+import uxEngEdit from "./edit/uxEng";
+import uxEngIndex from "./index/uxEng";
+import uxEngItem from "./item/uxEng";
+
+export default {
+  uxEngAdd, uxEngDetail, uxEngEdit, uxEngIndex, uxEngItem
+};
