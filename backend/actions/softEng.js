5fed146b5147cf71cf4c891d0e8806faba553d1a
WillJHaggard <willjhaggard@gmail.com>
2016-03-25 10:28:16 -0400

    Added backend feature structure

---
+++ backend/actions/softEng.js
@@ -0,0 +1,15 @@
+import indexRouter from "./index/softEng";
+import addRouter from "./add/softEng";
+import addOrEditRouter from "./add-or-edit/softEng";
+import removeRouter from "./remove/softEng";
+import randomRouter from "./random/softEng";
+import detailRouter from "./detail/softEng";
+
+export default [
+  indexRouter,
+  addRouter,
+  addOrEditRouter,
+  removeRouter,
+  randomRouter,
+  detailRouter
+];
---
+++ backend/actions/uxEng.js
@@ -0,0 +1,15 @@
+import indexRouter from "./index/uxEng";
+import addRouter from "./add/uxEng";
+import addOrEditRouter from "./add-or-edit/uxEng";
+import removeRouter from "./remove/uxEng";
+import randomRouter from "./random/uxEng";
+import detailRouter from "./detail/uxEng";
+
+export default [
+  indexRouter,
+  addRouter,
+  addOrEditRouter,
+  removeRouter,
+  randomRouter,
+  detailRouter
+];
